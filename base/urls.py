b6f37e4bce6c6adf35640b1144089c64345fb369
sheshikumar1 <sheshikumarerraboina@gmail.com>
2024-12-18 13:18:33 +0530

    new added

--- base/urls.py
+++ base/urls.py
@@ -5,6 +5,7 @@ urlpatterns=[
     path('',views.home,name="home"),
     path('login/',views.loginPage,name='login'),
     path('room/<str:pk>/',views.room,name="room"),
+    path('profile/<str:pk>/',views.userProfile,name='user-profile'),
     path('logout/',views.logoutUser,name='logout'),
     path('register/',views.registerPage,name='register'),
     path('create-room/',views.createRoom,name='create-room'),
--- base/views.py
+++ base/views.py
@@ -66,12 +66,13 @@ def home(request):
 
     topics=Topic.objects.all()
     room_count=rooms.count()
-    context={'rooms':rooms,'topics':topics,'room_count':room_count}
+    room_messages=Message.objects.filter(Q(room__topic__name__icontains=q))
+    context={'rooms':rooms,'topics':topics,'room_count':room_count,'room_messages':room_messages}
     return render(request,'base/home.html',context)
 
 def room(request, pk):
     room=Room.objects.get(id=pk)
-    room_messages=room.message_set.all().order_by('-created')
+    room_messages=room.message_set.all()
     participants=room.participants.all()
     
     if request.method=='POST':
@@ -83,8 +84,17 @@ def room(request, pk):
         room.participants.add(request.user)
         return redirect('room',pk=room.id)
 
-    context={'room': room,'room_messages': room_messages,'participants':participants}
+    context={'room': room,'room_messages': room_messages,'participants': participants}
     return render(request,'base/room.html',context)
+
+def userProfile(request,pk):
+    user=User.objects.get(id=pk)
+    rooms=user.room_set.all()
+    room_messages=user.message_set.all()
+    topics=Topic.objects.all()
+    context={'user': user,'rooms': rooms,'room_messages': room_messages,'topics': topics}
+    return render(request,'base/profile.html',context)
+
 
 @login_required(login_url='login')
 def createRoom(request):
@@ -93,7 +103,9 @@ def createRoom(request):
     if request.method=='POST':
         form=RoomForm(request.POST)
         if form.is_valid():
-            form.save()
+            room=form.save(commit=False)
+            room.host=request.user
+            room.save()
             return redirect('home')
 
 
